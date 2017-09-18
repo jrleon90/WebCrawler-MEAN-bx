f7c00a154d8f73e50ebf9afd33cf0d22daa84d49
Jose <jrleon90@gmail.com>
2017-09-17 19:12:17 -0500

    data loop length bug

--- src/api/twitter/twitter-api.js
+++ src/api/twitter/twitter-api.js
@@ -32,7 +32,7 @@ var getTwitterCount = (query) => {
               twitterCount: twitterCount,
               sortTwData: tweets,
             };
-
+            console.log('tw', twitterObj.sortTwData.length);
             resolve(twitterObj);
           }
         });
